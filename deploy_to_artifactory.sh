9a989540b0a2b30c5dd71deb8795d1532d332f89
Alexander <alexander.demidko@stitchfix.com>
2017-09-21 13:54:14 -0700

    Fixed artifactory deployment script

--- deploy_to_artifactory.sh
+++ deploy_to_artifactory.sh
@@ -5,7 +5,7 @@ cp settings.xml /tmp
 ./datahighwaycreds.sh
 
 # run mvn command
-mvn clean && mvn deploy --settings settings.xml
+mvn clean && mvn deploy -DaltDeploymentRepository=artifactory-releases::default::http://artifactory.vertigo.stitchfix.com/artifactory/releases -DaltReleaseDeploymentRepository=artifactory-releases::default::http://artifactory.vertigo.stitchfix.com/artifactory/releases -DaltSnapshotDeploymentRepository=artifactory-snapshots::default::http://artifactory.vertigo.stitchfix.com/artifactory/snapshots --settings settings.xml 
 
 # delete settings.xml with creds in it
 rm settings.xml
